28355f47ba28da344a4cbe45b2d30e77fbbef3a0
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2022-09-20 15:18:14 +0600

    setup-and-tour-schema-creation

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const app = require('./app')
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+    console.log("Database is connected. YEH!!".black.bgWhite.bold)
+})
+
+const port = process.env.PORT || 8080;
+
+
+app.listen(port, () => {
+    console.log(`App in working in ${port}`.black.bgYellow.bold)
+})
